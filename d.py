8ff6f355b2b7109a1416533cdfbe19e4dcced7e9
santosh kumar agepogu <santoshkumaragepogu484@gmail.com>
2025-03-17 01:00:38 -0600

    Add files via upload

---
+++ d.py
@@ -0,0 +1,138 @@
+# dictionary = {} #empty dictionary
+# print(type(dictionary))
+
+# dictionary = {
+#     1:"santosh",
+#     2:"agepogu",
+#     3:"ashok",
+#     4:"prasad",
+#     5:"rajasekhar",
+# }
+# print(dictionary)
+
+# dictionary = dict()
+# print(type(dictionary))
+
+# dictionary_1 = {
+#     "user1" : "user1@123",
+#     "user2" : "user2@123",
+#     "user3" : "user3@123",
+#     "user4" : "user4@123",
+# }
+# # dictionary_1.clear()
+# print(dictionary_1)
+# sample_copy = dict
+# print(dictionary_1.get("user5"))
+# print(dictionary_1["user5"])
+
+# list_1 = [1,2,3,4]
+# print(list_1[2])
+
+
+# dictionary_1 = {
+#     "user1" : "user1@123",
+#     "user2" : "user2@123",
+#     "user3" : "user3@123",
+#     "user4" : "user4@123",
+# }
+# print(dictionary_1.keys())
+# print(dictionary_1.values())
+# print(dictionary_1.items())
+# dictionary_1.pop("user1")
+# print(dictionary_1)
+
+
+# dictionary_1 = {
+#     "user1" : "user1@123",
+#     "user2" : "user2@123",
+#     "user3" : "user3@123",
+#     "user4" : "user4@123",
+# }
+
+# sample_1 = {
+#     "vasu":"vasu@123",
+#     "anil":"anil@123",
+# }
+
+# dictionary_1.update({1:"vasu",})
+# print(dictionary_1)
+# print(sample_1)
+
+# dictionary_1 = {
+#     "user1" : "santoshdpm1432@gmail.com",
+#     "user2" : "neehaswi@gmail.com",
+#     "user3" : "srinivasulu@gmail.com",
+#     "user4" : "santoshkumar_agepogu@gmail.com",
+# }
+# dictionary_1["user4"] = "pesalaashok@gamil.com"
+# dictionary_1["user5"] = "nagaprasad@gmail.com"
+# print(dictionary_1)
+
+# word = input("enter the word: ")
+# meaning = input("enter the meaning: ")
+# dictionary_1 = {}
+# dictionary_1[word] = meaning
+# print(dictionary_1)
+# happiness
+
+# user_input = input("enter some thing")
+# print(user_input)
+
+
+# task_1
+
+# Write Python code to access and print the value associated with the key 'price' in
+# the following dictionary:
+
+# # product_info = {'name': 'Laptop', 'brand': 'Dell', 'price': 1
+# 200}
+# Your code here
+# Output should be: 1200
+
+# product_info = {'name': 'Laptop', 'brand': 'Dell', 'price': 1200}
+# print(product_info.values())
+
+# product_info = {'name': 'Laptop', 'brand': 'Dell', 'price': 1200}
+# print(product_info['price'])
+
+# task_2
+
+# Write Python code to add a new key-value pair to the following dictionary:
+
+# my_dict = {'name': 'santosh', 'age': 25}
+# my_dict['city'] = 'nandyal'
+# print(my_dict)
+
+
+# task_3
+
+# Write Python code to remove the key-value pair with the key 'city' from the
+# following dictionary:
+
+
+# my_dict = {'name': 'santosh', 'age': 25, 'city' : 'nandyal' }
+# my_dict.pop('city')
+# print(my_dict)
+
+# task_4
+
+# Dictionary Keys
+# Write Python code to print all the keys present in the following dictionary:
+# my_dict = {'name': 'python', 'age': 25, 'city': 'Rajahmundr
+# y'}
+# Your code here
+# Output should be: ['name', 'age', 'city']
+
+# my_dict = {'name': 'santosh', 'age': 25, 'city' : 'nandyal' }
+# print(my_dict.keys())
+
+# task_5
+
+# Write Python code to print all the values present in the following dictionary:
+# Dictionary 
+
+# my_dict = {'name': 'python', 'age': 25, 'city': 'tanuku'}
+# Output should be: ['python', 25, 'tanuku']
+
+# my_dict = {'name': 'python', 'age': 25, 'city': 'tanuku'}
+# print(my_dict.values())
